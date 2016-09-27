5519c6387db94fe8d5589d23af64aeb02ee52316
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2016-09-27 23:07:34 +0200

    Initial commit
    - Basic project setup
    - Created StringExtensions with its first method
    - Set up test project

---
+++ VaraniumSharp.Tests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using VaraniumSharp.Extensions;
+
+namespace VaraniumSharp.Tests.Extensions
+{
+    public class StringExtensionsTest
+    {
+        [Test]
+        public void RetrieveStringKeyFromConfiguration()
+        {
+            // arrange
+            const string key = "StringValue";
+            const string expectedValue = "TestValue";
+
+            // act
+            var result = key.GetConfigurationValue<string>();
+
+            // assert
+            result.GetType().Should().Be<string>();
+            result.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void RetrieveIntegerKeyFromConfiguration()
+        {
+            // arrange
+            const string key = "IntValue";
+            const int expectedValue = 20787;
+
+            // act
+            var result = key.GetConfigurationValue<int>();
+
+            // assert
+            result.GetType().Should().Be<int>();
+            result.Should().Be(expectedValue);
+        }
+
+        [Test]
+        public void RetrieveEmptyKeyFromConfiguration()
+        {
+            // arrange
+            const string key = "Empty";
+
+            // act
+            var result = key.GetConfigurationValue<DateTime>();
+
+            // assert
+            result.Should().Be(default(DateTime));
+        }
+
+        [Test]
+        public void RetrieveKeyThatDoesNotExistFromConfiguration()
+        {
+            // arrange
+            const string key = "InvalidKey";
+            var action = new Action(() => key.GetConfigurationValue<string>());
+
+            // act
+            // assert
+            action.ShouldThrow<KeyNotFoundException>();
+        }
+    }
+}
---
+++ VaraniumSharp/Extensions/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace VaraniumSharp.Extensions
+{
+    /// <summary>
+    /// Extension methods for strings
+    /// </summary>
+    public static class StringExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Read a value from App.config and return it as the spesified type
+        /// </summary>
+        /// <typeparam name="T">Type of the value that is to be retrieved</typeparam>
+        /// <param name="key">Key for which the value should be retrieved</param>
+        /// <returns>Value from App.config that is associated with the key</returns>
+        public static T GetConfigurationValue<T>(this string key)
+        {
+            if (!ConfigurationManager.AppSettings.AllKeys.Contains(key))
+            {
+                throw new KeyNotFoundException($"Could not locate key {key} in the configuration");
+            }
+            
+            var value = ConfigurationManager.AppSettings[key];
+            if (string.IsNullOrEmpty(value))
+            {
+                return default(T);
+            }
+            var typedValue = (T)Convert.ChangeType(value, typeof(T));
+            return typedValue;
+        }
+
+        #endregion
+    }
+}
