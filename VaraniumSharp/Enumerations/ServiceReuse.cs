83a48f78e3e82b2774dfc2eb068ea24c7e095ef6
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2016-10-02 00:28:04 +0200

    feat: Add classes to support automatic Container registration
    - Added AutomaticContainerRegistrationAttribute class
    - Added abstract class to assist in building automatic container registrations classes in other projects

---
+++ VaraniumSharp/Enumerations/ServiceReuse.cs
@@ -0,0 +1,17 @@
+﻿namespace VaraniumSharp.Enumerations
+{
+    /// <summary>
+    /// Enumerations that indicate service reuseability
+    /// </summary>
+    public enum ServiceReuse
+    {
+        /// <summary>
+        /// Set up the service with default reuse
+        /// </summary>
+        Default,
+        /// <summary>
+        /// Set up the service with singleton reuse
+        /// </summary>
+        Singleton
+    }
+}
