333f3f828dbb70db9760061bce00b89d4f1fcc12
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2016-11-15 23:22:44 +0200

    feat: Add AutomaticConcretionContainerRegistration
    - Added new AutomaticConcretionContainerRegistrationAttribute. Closes #10

---
+++ VaraniumSharp.Tests/Attributes/AutomaticConcretionContainerRegistrationTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using VaraniumSharp.Attributes;
+using VaraniumSharp.Enumerations;
+
+namespace VaraniumSharp.Tests.Attributes
+{
+    public class AutomaticConcretionContainerRegistrationTests
+    {
+        [Test]
+        public void ReadCustomAttributeForDefaultSetup()
+        {
+            // arrange
+            // act
+            var sut = new IheritorDummy();
+
+            // assert
+            var attribute =
+                (AutomaticConcretionContainerRegistrationAttribute)
+                sut.GetType()
+                    .GetCustomAttributes(typeof(AutomaticConcretionContainerRegistrationAttribute), true)
+                    .FirstOrDefault();
+            attribute.Should().NotBeNull();
+            attribute?.Reuse.Should().Be(ServiceReuse.Default);
+        }
+
+        [Test]
+        public void ReadCustomAttributeForSingletonSetup()
+        {
+            // arrange
+            // act
+            // assert
+            var attribute =
+                (AutomaticConcretionContainerRegistrationAttribute)
+                typeof(IInterfaceDummy)
+                    .GetCustomAttributes(typeof(AutomaticConcretionContainerRegistrationAttribute), true)
+                    .FirstOrDefault();
+            attribute.Should().NotBeNull();
+            attribute?.Reuse.Should().Be(ServiceReuse.Singleton);
+        }
+
+        [AutomaticConcretionContainerRegistration]
+        private class BaseClassDummy
+        {}
+
+        private class IheritorDummy : BaseClassDummy
+        { }
+
+        [AutomaticConcretionContainerRegistration(Reuse = ServiceReuse.Singleton)]
+        private interface IInterfaceDummy
+        {}
+    }
+}
---
+++ VaraniumSharp/Attributes/AutomaticConcretionContainerRegistrationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using VaraniumSharp.Enumerations;
+
+namespace VaraniumSharp.Attributes
+{
+    /// <summary>
+    /// Used to register all classes or structs that implement a specific interface or inherits from a base class.
+    /// This attribute should be applied to the interface or base class that the class should be registered as in the Container
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct)]
+    public sealed class AutomaticConcretionContainerRegistrationAttribute : Attribute
+    {
+        #region Properties
+
+        /// <summary>
+        /// How the serivce should be setup for reuse
+        /// </summary>
+        public ServiceReuse Reuse { get; set; }
+
+        #endregion Properties
+    }
+}
