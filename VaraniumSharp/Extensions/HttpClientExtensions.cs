15540602c53be95ce9cb7bed8e950a0c04464bee
Ralph Viktor <ralphviktor@ninetaillabs.com>
2017-03-11 15:53:31 +0200

    feat: Basic Authentication extension. Fixes #31
    - Added basic authentication header extension

---
+++ VaraniumSharp/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace VaraniumSharp.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpClient"/>
+    /// </summary>
+    public static class HttpClientExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Set Basic Authentication header
+        /// </summary>
+        /// <param name="client">HttpClient for which the header should be set</param>
+        /// <param name="username">Username for authentication</param>
+        /// <param name="password">Password for authentication</param>
+        /// <returns>HttpClient</returns>
+        public static HttpClient SetBasicAuthHeader(this HttpClient client, string username, string password)
+        {
+            var credentials = Convert.ToBase64String(Encoding.Default.GetBytes($"{username}:{password}"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+            return client;
+        }
+
+        #endregion
+    }
+}
