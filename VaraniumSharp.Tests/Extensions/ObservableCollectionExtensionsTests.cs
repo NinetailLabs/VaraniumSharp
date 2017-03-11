766a29593d3da96191a6bad6e56dad42ba7ef645
Ralph Viktor <ralphviktor@ninetaillabs.com>
2017-03-11 11:12:44 +0200

    feat: ObservableCollection Clear extension. Fixes #28
    - Added extension method that allows executing an action against the items of an ObservableCollection prior to clearing the collection

---
+++ VaraniumSharp.Tests/Extensions/ObservableCollectionExtensionsTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.ObjectModel;
+using VaraniumSharp.Extensions;
+
+namespace VaraniumSharp.Tests.Extensions
+{
+    public class ObservableCollectionExtensionsTests
+    {
+        #region Public Methods
+
+        [Test]
+        public void CleaningCollectionInvokesAction()
+        {
+            // arrange
+            var itemDummy = new CollectionContentFixture();
+            var sut = new ObservableCollection<CollectionContentFixture> { itemDummy };
+            var helper = new CleanupHelper();
+
+            // act
+            sut.Clear(helper.ClearAction);
+
+            // assert
+            helper.WasInvoked.Should().BeTrue();
+            helper.CollectionPassedIn.Should().BeSameAs(sut);
+        }
+
+        [Test]
+        public void ClearingCollectionExecutesActionAgainstItemsInTheCollectionPriorToClearingTheCollection()
+        {
+            // arrange
+            var itemDummy = new CollectionContentFixture();
+            var sut = new ObservableCollection<CollectionContentFixture> { itemDummy };
+            var helper = new CleanupHelper();
+
+            // act
+            sut.Clear(helper.ClearAction);
+
+            // assert
+            itemDummy.CleanupWasInvoked.Should().BeTrue();
+        }
+
+        [Test]
+        public void CollectionIsCleared()
+        {
+            // arrange
+            var itemDummy = new CollectionContentFixture();
+            var sut = new ObservableCollection<CollectionContentFixture> { itemDummy };
+            var helper = new CleanupHelper();
+
+            // act
+            sut.Clear(helper.ClearAction);
+
+            // assert
+            sut.Count.Should().Be(0);
+        }
+
+        #endregion
+
+        private class CleanupHelper
+        {
+            #region Properties
+
+            public ObservableCollection<CollectionContentFixture> CollectionPassedIn { get; private set; }
+
+            public bool WasInvoked { get; private set; }
+
+            #endregion
+
+            #region Public Methods
+
+            public void ClearAction(ObservableCollection<CollectionContentFixture> collection)
+            {
+                CollectionPassedIn = collection;
+                foreach (var item in collection)
+                {
+                    item.DoCleanup();
+                }
+                WasInvoked = true;
+            }
+
+            #endregion
+        }
+
+        private class CollectionContentFixture
+        {
+            #region Properties
+
+            public bool CleanupWasInvoked { get; private set; }
+
+            #endregion
+
+            #region Public Methods
+
+            public void DoCleanup()
+            {
+                CleanupWasInvoked = true;
+            }
+
+            #endregion
+        }
+    }
+}
