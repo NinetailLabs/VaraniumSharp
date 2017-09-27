4087bee52fa2f18de9ae703bb4b3075f26c444af
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2017-09-27 14:59:45 +0000

    feat: StaticHelper. Closes #43
    - Added StaticHelper class with method to invoke Process.Start() with a filename
    - Updated Nuspec release notes

---
+++ VaraniumSharp/GenericHelpers/StaticMethodWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using VaraniumSharp.Attributes;
+using VaraniumSharp.Interfaces.GenericHelpers;
+
+namespace VaraniumSharp.GenericHelpers
+{
+    /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
+    [AutomaticContainerRegistration(typeof(IStaticMethodWrapper))]
+    public class StaticMethodWrapper : IStaticMethodWrapper
+    {
+        #region Public Methods
+
+        /// <inheritdoc />
+        public void StartProcess(string filename)
+        {
+            Process.Start(filename);
+        }
+
+        #endregion
+    }
+}
