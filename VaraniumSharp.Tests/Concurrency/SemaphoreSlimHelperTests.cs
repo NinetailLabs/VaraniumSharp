d4504c1a5c640d2fee95931b902c8b819bd8335b
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2018-12-03 17:15:43 +0200

    feat: SemaphoreSlim wrapper. Closes #60
    - Added SemaphoreSlimHelper that wraps fine-grained Semaphore access

---
+++ VaraniumSharp.Tests/Concurrency/SemaphoreSlimHelperTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VaraniumSharp.Concurrency;
+using Xunit;
+
+namespace VaraniumSharp.Tests.Concurrency
+{
+    public class SemaphoreSlimHelperTests
+    {
+        [Fact]
+        public async Task IfSemaphoreDoesNotExistOneIsCreated()
+        {
+            // arrange
+            const int key = 1;
+            var sut = new SemaphoreSlimHelper<int>();
+
+            // act
+            var semaphore = await sut.AcquireSemaphoreSlimAsync(key);
+
+            // assert
+            sut.NumberOfSemaphoresIssued.Should().Be(1);
+            semaphore.Dispose();
+        }
+
+        [Fact]
+        public void IfSemaphoreDoesNotExistOneWillBeCreatedNonAsync()
+        {
+            // arrange
+            const int key = 1;
+            var sut = new SemaphoreSlimHelper<int>();
+
+            // act
+            var semaphore = sut.AcquireSemaphoreSlim(key);
+
+            // assert
+            sut.NumberOfSemaphoresIssued.Should().Be(1);
+            semaphore.Dispose();
+        }
+
+        [Fact]
+        public async Task IfSemaphoreIsAlreadyInUsedNextCallBlocks()
+        {
+            // arrange
+            const int key = 1;
+            var sut = new SemaphoreSlimHelper<int>();
+            await sut.AcquireSemaphoreSlimAsync(key);
+
+            // act
+            var secondSemaphore = sut.AcquireSemaphoreSlimAsync(key);
+
+            // assert
+            secondSemaphore.Status.Should().Be(TaskStatus.WaitingForActivation);
+            sut.NumberOfSemaphoresIssued.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task IfSemaphoreIsDisposedAndThereAreNoWaitingMethodsTheEntryIsRemovedFromTheDictionary()
+        {
+            // arrange
+            const int key = 1;
+            var sut = new SemaphoreSlimHelper<int>();
+
+            var semaphore = await sut.AcquireSemaphoreSlimAsync(key);
+
+            // act
+            semaphore.Dispose();
+
+            // assert
+            sut.NumberOfSemaphoresIssued.Should().Be(0);
+        }
+
+        [Fact]
+        public async Task IfSemaphoreIsDisposedItIsCorrectlyReleased()
+        {
+            // arrange
+            const int key = 1;
+            var sut = new SemaphoreSlimHelper<int>();
+            var semaphore = await sut.AcquireSemaphoreSlimAsync(key);
+            var secondSemaphore = sut.AcquireSemaphoreSlimAsync(key);
+
+            // act
+            semaphore.Dispose();
+            Thread.Sleep(250);
+
+            // assert
+            secondSemaphore.Status.Should().Be(TaskStatus.RanToCompletion);
+        }
+    }
+}
---
+++ VaraniumSharp/Concurrency/DisposableSemaphoreSlim.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace VaraniumSharp.Concurrency
+{
+    /// <summary>
+    /// Disposable SemaphoreSlim wrapper for use with <see cref="SemaphoreSlimHelper{T}"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class DisposableSemaphoreSlim<T> : IDisposable
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Construct with required values
+        /// </summary>
+        /// <param name="key">Key the Semaphore was acquired with</param>
+        /// <param name="semaphore">The SemaphoreSlim for the instance</param>
+        /// <param name="disposalAction">Action that will be invoked when the instance is disposed</param>
+        internal DisposableSemaphoreSlim(T key, SemaphoreSlim semaphore, Action<T> disposalAction)
+        {
+            _key = key;
+            _semaphore = semaphore;
+            _disposalAction = disposalAction;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _disposalAction.Invoke(_key);
+            _semaphore.Release();
+        }
+
+        #endregion
+
+        #region Variables
+
+        /// <summary>
+        /// Action that will be invoked on disposal to notify <see cref="SemaphoreSlimHelper{T}"/> of disposal
+        /// </summary>
+        private readonly Action<T> _disposalAction;
+
+        /// <summary>
+        /// Key the SemaphoreSlim was acquired with
+        /// </summary>
+        private readonly T _key;
+
+        /// <summary>
+        /// SemaphoreSlim for this instance
+        /// </summary>
+        private readonly SemaphoreSlim _semaphore;
+
+        #endregion
+    }
+}
