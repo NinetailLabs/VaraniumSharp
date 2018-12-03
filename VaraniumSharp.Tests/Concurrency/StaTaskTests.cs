98ab05394bbe62b5b148db6c4e9685185b7f4aa2
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2018-12-03 17:00:46 +0200

    feat: StaTasks. Closes #59
    - Implemented StaTaskSchedular to allow scheduling Tasks with STA thread
    - Added StaTask wrapper that allow Task like usage of STA tasks

---
+++ VaraniumSharp.Tests/Concurrency/StaTaskTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VaraniumSharp.Concurrency;
+using Xunit;
+
+namespace VaraniumSharp.Tests.Concurrency
+{
+    public class StaTaskTests
+    {
+        [Fact]
+        public void CancellationTokenIsPassedToTheTask()
+        {
+            // arrange
+            var wasExecuted = false;
+            var act = new Action(() => { wasExecuted = true; });
+            var cancellationToken = new CancellationToken(true);
+            var task = StaTask.Run(act, cancellationToken);
+            var taskAct = new Action(() => task.Start());
+
+            // act
+            // assert
+            wasExecuted.Should().BeFalse();
+            taskAct.Should().Throw<Exception>();
+            task.Status.Should().Be(TaskStatus.Canceled);
+        }
+
+        [Fact]
+        public async Task TaskIsCorrectlyScheduledOnStaThread()
+        {
+            // arrange
+            ApartmentState? receivedState = null;
+            var act = new Action(() => { receivedState = Thread.CurrentThread.GetApartmentState(); });
+
+            // act
+            await StaTask.Run(act);
+
+            // assert
+            receivedState.Should().NotBeNull();
+            receivedState.Should().Be(ApartmentState.STA);
+        }
+    }
+}
---
+++ VaraniumSharp/Concurrency/StaTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VaraniumSharp.Concurrency
+{
+    /// <summary>
+    /// Helper for executing asynchronous operations on <see cref="ApartmentState.STA"/> threads
+    /// </summary>
+    public static class StaTask
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        static StaTask()
+        {
+            Scheduler = new StaTaskScheduler();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Queues an asynchronous action on a <see cref="ApartmentState.STA"/> thread and returns a <see cref="Task"/> that represents that work
+        /// </summary>
+        /// <param name="action">The work to execute asynchronously</param>
+        /// <returns>Representation of asynchronous work</returns>
+        public static Task Run(Action action)
+        {
+            return Run(action, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Queues an asynchronous action on a <see cref="ApartmentState.STA"/> thread and returns a <see cref="Task"/> that represents that work
+        /// </summary>
+        /// <param name="action">The work to execute asynchronously</param>
+        /// <param name="cancellationToken">Token that can be used to cancel the execution of the action</param>
+        /// <returns>Representation of asynchronous work</returns>
+        public static Task Run(Action action, CancellationToken cancellationToken)
+        {
+            return new TaskFactory().StartNew(action, cancellationToken, TaskCreationOptions.None, Scheduler);
+        }
+
+        #endregion
+
+        #region Variables
+
+        /// <summary>
+        /// StaTaskScheduler instance
+        /// </summary>
+        private static readonly StaTaskScheduler Scheduler;
+
+        #endregion
+    }
+}
