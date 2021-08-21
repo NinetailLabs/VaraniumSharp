46f57d4313d88cab9df46c6f2ce5942eda15700f
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-08-21 10:04:43 +0200

    feat: DI Factory wrapper. Closes #75
    - Added interface to allow level 2 libraries to access DI in a IoC independant way.

---
+++ VaraniumSharp/Interfaces/DependencyInjection/IContainerFactoryWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace VaraniumSharp.Interfaces.DependencyInjection
+{
+    /// <summary>
+    /// Wrapper for the DI container.
+    /// This provides an abstracted way to resolve dependencies from the DI system without having to directly reference the container in your code.
+    /// This interface is meant to provide factories in level 2 VaraniumSharp libraries access to the container without the code having to take a direct dependency on the IoC library.
+    /// </summary>
+    public interface IContainerFactoryWrapper
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Resolve a Service from the Container
+        /// </summary>
+        /// <typeparam name="TService">Service to resolve</typeparam>
+        /// <returns>Resolved service</returns>
+        TService Resolve<TService>();
+
+        /// <summary>
+        /// Resolve Services from the container via a shared interface of parent class
+        /// </summary>
+        /// <typeparam name="TService">Interface or parent class that children are registered under</typeparam>
+        /// <returns>Collection of children classes that inherit from the parent or implement the interface</returns>
+        IEnumerable<TService> ResolveMany<TService>();
+
+        #endregion
+    }
+}
