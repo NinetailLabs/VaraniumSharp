56d7f4ba199cc65344d7b5a6c8da005b2d60ab5d
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-09-10 15:00:43 +0200

    feat: Directory Wrapper. Closes #78
    - Added directory wrapper
    - Added unit tests for directory wrapper

---
+++ VaraniumSharp/Wrappers/DirectoryWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using VaraniumSharp.Attributes;
+using VaraniumSharp.Enumerations;
+using VaraniumSharp.Interfaces.Wrappers;
+
+namespace VaraniumSharp.Wrappers
+{
+    /// <summary>
+    /// Assist with Directory operations
+    /// </summary>
+    [AutomaticContainerRegistration(typeof(IDirectoryWrapper), ServiceReuse.Singleton)]
+    public class DirectoryWrapper : IDirectoryWrapper
+    {
+        #region Public Methods
+
+        /// <inheritdoc />
+        public DirectoryInfo CreateDirectory(string path)
+        {
+            return Directory.CreateDirectory(path);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<string> EnumerateFiles(string path)
+        {
+            return Directory.EnumerateFiles(path);
+        }
+
+        /// <inheritdoc />
+        public bool Exists(string folderPath)
+        {
+            return Directory.Exists(folderPath);
+        }
+
+        #endregion
+    }
+}
