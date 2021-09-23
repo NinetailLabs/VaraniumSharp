e6b84c47199be27f27a59e12ca34e91d61a390a5
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-09-23 14:36:06 +0200

    Reference counting cache. #83
    - Ported ReferenceCountingCache from ACM-NG
    - Ported unit test from ACM-NG
    - Added interface for the reference counting cache

---
+++ VaraniumSharp.Tests/Caching/ReferenceCountingCacheBaseTests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+using FluentAssertions;
+using VaraniumSharp.Caching;
+using VaraniumSharp.Interfaces.Caching;
+using Xunit;
+
+namespace VaraniumSharp.Tests.Caching
+{
+    public class ReferenceCountingCacheBaseTests
+    {
+        private class ReferenceCountingBaseFixture : ReferenceCountingCacheBase<CacheEntityFixture>
+        {
+            #region Constructor
+
+            public ReferenceCountingBaseFixture(TimeSpan cacheRetentionPeriod)
+                : base(cacheRetentionPeriod)
+            { }
+
+            #endregion
+
+            #region Public Methods
+
+            public override async Task<CacheEntityFixture> RetrieveEntryItemAsync(int key)
+            {
+                var stringKey = key.ToString();
+                IncreaseReferenceCount(key);
+                return await EntryCache.GetAsync(stringKey);
+            }
+
+            public override async Task<List<CacheEntityFixture>> RetrieveEntryItemsAsync(List<int> keys)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override async Task<bool> SaveReadmodelAsync(IEntity entry)
+            {
+                // Does nothing for this fixture
+                await Task.Delay(1);
+                return true;
+            }
+
+            public override async Task<Dictionary<int, bool>> SaveReadmodelsAsync(List<IEntity> entries)
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+
+            #region Private Methods
+
+            protected override void CacheItemRemoved(CacheEntryRemovedArguments removedEntryDetails)
+            {
+                if (removedEntryDetails.CacheItem.Value is CacheEntityFixture entry)
+                {
+                    entry.CacheItemRemoved();
+                }
+            }
+
+            protected override async Task<Dictionary<string, CacheEntityFixture>> RetrieveEntriesFromRepositoryAsync(List<string> keys)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override async Task<CacheEntityFixture> RetrieveEntryFromRepositoryAsync(string key)
+            {
+                await Task.Delay(1);
+                if (int.TryParse(key, out var intKey))
+                {
+                    return new CacheEntityFixture(intKey);
+                }
+
+                throw new KeyNotFoundException("Key could not be found");
+            }
+
+            #endregion
+        }
+
+        private class CacheEntityFixture : ICacheEntry
+        {
+            #region Constructor
+
+            public CacheEntityFixture(int id)
+            {
+                Id = id;
+            }
+
+            #endregion
+
+            #region Properties
+
+            public int CacheItemRemovedCallCount { get; private set; }
+
+            public int Id { get; }
+
+            #endregion
+
+            #region Public Methods
+
+            public void CacheItemRemoved()
+            {
+                CacheItemRemovedCallCount++;
+            }
+
+            #endregion
+        }
+
+        [Fact]
+        public async Task IfAnEntryIsNoLongerUsedItIsRemovedFromTheCache()
+        {
+            // arrange
+            const int key = 12;
+            var cacheCleanDuration = TimeSpan.FromMilliseconds(200);
+            using var sut = new ReferenceCountingBaseFixture(cacheCleanDuration);
+            var entry = await sut.RetrieveEntryItemAsync(key);
+
+            // act
+            sut.EntryNoLongerUsed(key);
+            await Task.Delay(TimeSpan.FromMilliseconds(600));
+
+            // assert
+            entry.CacheItemRemovedCallCount.Should().Be(1);
+        }
+
+        [Fact]
+        public async Task IfAnEntryWasRemovedFromTheCacheItIsRetrievedFromTheDataStoreOnASubsequentRequest()
+        {
+            // arrange
+            const int key = 12;
+            var cacheCleanDuration = TimeSpan.FromMilliseconds(200);
+            using var sut = new ReferenceCountingBaseFixture(cacheCleanDuration);
+            var entry = await sut.RetrieveEntryItemAsync(key);
+            sut.EntryNoLongerUsed(key);
+            await Task.Delay(TimeSpan.FromMilliseconds(600));
+
+            // act
+            var newEntry = await sut.RetrieveEntryItemAsync(key);
+
+            // assert
+            entry.Should().NotBe(newEntry);
+        }
+
+        [Fact]
+        public async Task IfEntryIsNotMarkedAsNotUsedItIsNotRemovedWhenTheCacheIsCleared()
+        {
+            // arrange
+            const int key = 12;
+            var cacheCleanDuration = TimeSpan.FromMilliseconds(200);
+            using var sut = new ReferenceCountingBaseFixture(cacheCleanDuration);
+
+            // act
+            var entry = await sut.RetrieveEntryItemAsync(key);
+            await Task.Delay(TimeSpan.FromMilliseconds(600));
+
+            // assert
+            entry.CacheItemRemovedCallCount.Should().Be(0);
+        }
+
+        [Fact]
+        public async Task IfMultipleReferencesAreHeldAndOneIsReleasedTheEntryIsNotRemovedFromTheCache()
+        {
+            // arrange
+            const int key = 12;
+            var cacheCleanDuration = TimeSpan.FromMilliseconds(200);
+            using var sut = new ReferenceCountingBaseFixture(cacheCleanDuration);
+            var entry = await sut.RetrieveEntryItemAsync(key);
+            await sut.RetrieveEntryItemAsync(key);
+
+            // act
+            sut.EntryNoLongerUsed(key);
+            await Task.Delay(TimeSpan.FromMilliseconds(600));
+
+            // assert
+            entry.CacheItemRemovedCallCount.Should().Be(0);
+        }
+    }
+}
---
+++ VaraniumSharp/Caching/ReferenceCountingCache.cs
@@ -0,0 +1,132 @@
+﻿#if NETSTANDARD2_1_OR_GREATER
+#nullable enable
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using VaraniumSharp.Interfaces.Caching;
+
+namespace VaraniumSharp.Caching
+{
+    /// <summary>
+    /// Cache that uses counts of items in use to decide when items should be evicted
+    /// </summary>
+    public abstract class ReferenceCountingCacheBase<T> : CacheBase<T>, IReferenceCountingCache, IDisposable where T : class, ICacheEntry
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Construct with a logger
+        /// </summary>
+        /// <param name="cacheRetentionPeriod">Timespan the cache will wait before purging un-referenced entries</param>
+        protected ReferenceCountingCacheBase(TimeSpan cacheRetentionPeriod)
+        {
+            ItemReferenceCount = new ConcurrentDictionary<int, int>();
+            _cacheCleanupTimer = new Timer(CacheCleanupCallback, null, cacheRetentionPeriod, cacheRetentionPeriod);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc />
+        public void EntryNoLongerUsed(int entityId)
+        {
+            if (ItemReferenceCount.ContainsKey(entityId))
+            {
+                ItemReferenceCount[entityId] -= 1;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Occurs when the <see cref="_cacheCleanupTimer"/> fires
+        /// </summary>
+        /// <param name="state">Always null</param>
+        protected virtual async void CacheCleanupCallback(object? state)
+        {
+            try
+            {
+                await ReferenceCleanupSemaphore
+                    .WaitAsync()
+                    .ConfigureAwait(false);
+
+                var itemsToClean = ItemReferenceCount.Where(x => x.Value <= 0).ToList();
+                foreach (var (id, _) in itemsToClean)
+                {
+                    var key = id.ToString(CultureInfo.InvariantCulture);
+                    await EntryCache
+                        .RemoveFromCacheAsync(key)
+                        .ConfigureAwait(false);
+                    ItemReferenceCount.Remove(id, out _);
+                }
+            }
+            finally
+            {
+                ReferenceCleanupSemaphore.Release();
+            }
+        }
+
+        /// <summary>
+        /// Performs tasks related to freeing resources etc
+        /// </summary>
+        /// <param name="disposing">Indicate if the class is being disposed</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _cacheCleanupTimer.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Increase the reference count for an AniList Id
+        /// </summary>
+        /// <param name="entityId">Id for which the reference count should be increased</param>
+        protected void IncreaseReferenceCount(int entityId)
+        {
+            if (ItemReferenceCount.ContainsKey(entityId))
+            {
+                ItemReferenceCount[entityId] += 1;
+            }
+            else
+            {
+                ItemReferenceCount.TryAdd(entityId, 1);
+            }
+        }
+
+        #endregion
+
+        #region Variables
+
+        /// <summary>
+        /// Timer used to handle cache cleanup operations
+        /// </summary>
+        private readonly Timer _cacheCleanupTimer;
+
+        /// <summary>
+        /// Dictionary used to count the number of classes that hold a specific item instance
+        /// </summary>
+        protected readonly ConcurrentDictionary<int, int> ItemReferenceCount;
+
+        /// <summary>
+        /// Semaphore used to lock <see cref="CacheCleanupCallback"/>
+        /// </summary>
+        protected readonly SemaphoreSlim ReferenceCleanupSemaphore = new SemaphoreSlim(1);
+
+        #endregion
+    }
+}
+#endif
